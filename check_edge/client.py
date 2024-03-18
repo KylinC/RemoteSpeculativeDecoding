3169f0b8ba574b32c380c61b61faec7e68b4a665
qilin4090 <qcql@me.com>
2024-03-18 14:31:39 +0800

    update for hogura

--- check_edge/client.py
+++ check_edge/client.py
@@ -5,6 +5,8 @@ import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from utils import norm_logits, sample, max_fn
 
+logging.basicConfig(level=logging.INFO)
+
 class ClientModel:
     def __init__(self, server_url:str=None, model_name:str="bigscience/bloom-560m")->None:
         self.server_url = server_url
@@ -18,45 +20,59 @@ class ClientModel:
         self.server_url = server_url
         return self.server_url!=None
         
-    def generate(self, prefix:str, max_len:int=50, num_return_sequences:int=1)->str:
-        input_ids = self._tokenizer.encode(prefix, return_tensors='pt').to(self._device)
+    def generate(self, prompt:str, max_len:int=50, num_return_sequences:int=1)->str:
+        input_ids = self._tokenizer.encode(prompt, return_tensors='pt').to(self._device)
         output = self._model.generate(input_ids, max_length=max_len, num_return_sequences=num_return_sequences)
         generated_text = self._tokenizer.decode(output[0], skip_special_tokens=True)
         return generated_text
     
-    def generate_from_server(self, prefix:str, max_len:int=50, num_return_sequences:int=1)->str:
-        data = {"prompt": prefix, "max_length": max_len, "num_return_sequences": num_return_sequences}
-        response = requests.post(f"{self.server_url}/predict", json=data)
-        generated_text = response.json()
+    def generate_from_server(self, prompt:str, max_len:int=50, num_return_sequences:int=1)->str:
+        data = {"prompt": prompt, "max_length": max_len, "num_return_sequences": num_return_sequences}
+        response = requests.post(f"{self.server_url}/generate", json=data)
+        generated_text = response.json()["generated_text"]
         return generated_text
     
-    def generate_with_server(self, prefix:str,  max_len : int = 20 , gamma : int = 4,
+    def generate_with_server(self, prompt:str,  max_len : int = 20 , gamma : int = 4,
                          temperature : float = 1, top_k : int = 0, top_p : float = 0, random_seed : int = None)->str:
-        prefix = self._tokenizer.encode(prefix, return_tensors='pt').to(self._device)
+        prefix = self._tokenizer.encode(prompt, return_tensors='pt').to(self._device)
+        output = self._spec_smapling(prefix, max_len, gamma, temperature, top_k, top_p, random_seed)
+        generated_text = self._tokenizer.decode(output[0], skip_special_tokens=True)
+        return generated_text
+    
+    # verify on server
+    def generate_with_server_check(self, prompt:str,  max_len : int = 20 , gamma : int = 4,
+                         temperature : float = 1, top_k : int = 0, top_p : float = 0, random_seed : int = None)->str:
+        prefix = self._tokenizer.encode(prompt, return_tensors='pt')
+        output = self._spec_tokens(prefix, max_len, gamma, temperature, top_k, top_p, random_seed)
+        generated_text = self._tokenizer.decode(output[0], skip_special_tokens=True)
+        return generated_text
+        
+    def _spec_smapling(self, prefix:torch.Tensor,  max_len : int = 20 , gamma : int = 4,
+                         temperature : float = 1, top_k : int = 0, top_p : float = 0, random_seed : int = None)->torch.Tensor:
         seq_len = prefix.shape[1]
         T = seq_len + max_len
         
         assert prefix.shape[0] == 1, "input batch size must be 1"
 
         while prefix.shape[1] < T:
-            # q = M_q[prefix + x_0, x_1, .., x_(gamma-2)]
+            # p = M_p[prefix + x_0, x_1, .., x_(gamma-2)]
             x = prefix
             prefix_len = prefix.shape[1]
             for _ in range(gamma):
                 # p.logits shape (batch, seq, vocab)
-                q = self._model(x).logits
-                next_tok = sample(norm_logits(q[:, -1, :], 
+                p = self._model(x).logits
+                next_tok = sample(norm_logits(p[:, -1, :], 
                                 temperature, top_k, top_p))
                 x = torch.cat((x, next_tok), dim=1)
             
             # normalize the logits
-            for i in range(q.shape[1]):
-                q[:,i,:] = norm_logits(q[:,i,:],
-                                temperature, top_k, top_p)
-            # p  = M_p[prefix + x_0, x_0, .., x_(gamma-1)]
-            p = self._spec_logits(x)
             for i in range(p.shape[1]):
                 p[:,i,:] = norm_logits(p[:,i,:],
+                                temperature, top_k, top_p)
+            # q  = M_q[prefix + x_0, x_0, .., x_(gamma-1)]
+            q = self._spec_logits(x)
+            for i in range(q.shape[1]):
+                q[:,i,:] = norm_logits(q[:,i,:],
                                 temperature, top_k, top_p)
 
             # n the end position of the valid prefix
@@ -67,36 +83,45 @@ class ClientModel:
             for i in range(gamma):
                 if random_seed:
                     torch.manual_seed(random_seed)
-                r = torch.rand(1, device = p.device)
+                r = torch.rand(1, device = self._device)
                 j = x[:, prefix_len + i]
                 
-                if r < torch.min(torch.tensor([1], device=q.device), p[:, prefix_len + i - 1, j] / q[:, prefix_len + i - 1, j]):
+                if r < torch.min(torch.tensor([1], device=self._device), q[:, prefix_len + i - 1, j] / p[:, prefix_len + i - 1, j]):
                     # accept, and update n
                     n += 1
                 else:
                     # reject
-                    t = sample(max_fn(p[:, n, :] - q[:, n, :]))
+                    t = sample(max_fn(q[:, n, :] - p[:, n, :]))
                     is_all_accept = False
                     break
         
             prefix = x[:, :n + 1]
             
             if is_all_accept:
-                t = sample(p[:, -1, :])
+                t = sample(q[:, -1, :])
             
             prefix = torch.cat((prefix, t), dim=1)
-
+            
         return prefix
 
+    def _spec_tokens(self, prefix:torch.Tensor,  max_len : int = 20 , gamma : int = 4,
+                         temperature : float = 1, top_k : int = 0, top_p : float = 0, random_seed : int = None)->torch.Tensor:
+        data = {"prefix": prefix.tolist()}
+        # response = requests.post(f"{self.server_url}/spec_logits", json=data)
+        # processed_tensor = torch.tensor(response.json()["logits"]).to(self._device)
+        # print(processed_tensor.shape)
+        # return processed_tensor
+        
     def _spec_logits(self, tensor:torch.Tensor)->torch.Tensor:
         data = {"ids": tensor.tolist()}
         response = requests.post(f"{self.server_url}/spec_logits", json=data)
-        processed_tensor = torch.tensor(response.json()["logits"])
+        processed_tensor = torch.tensor(response.json()["logits"]).to(self._device)
         print(processed_tensor.shape)
         return processed_tensor
     
-model = ClientModel(server_url="116.62.162.232:5000",model_name="/Users/kylinchan/models/bloom-560m")
-print(model.generate_with_server("The quick brown fox jumps over the lazy dog"))
+model = ClientModel(server_url="http://192.168.31.230:5000",model_name="bigscience/bloom-560m")
+# print(model.generate_from_server("Are you ok?"))
+import IPython; IPython.embed(); exit(1)
 
 # def send_tensor_to_server(tensor, server_url):
 #     data = {"tensor": tensor.tolist()}
--- check_edge/server.py
+++ check_edge/server.py
@@ -11,6 +11,7 @@ class ServerModel:
         CORS(self.app)
         self.app.route("/process_tensor", methods=["POST"])(self.process_tensor)
         self.app.route("/spec_logits", methods=["POST"])(self.spec_logits)
+        self.app.route("/generate", methods=["POST"])(self.generate_to_client)
         # device
         self._device = 'cuda' if torch.cuda.is_available() else 'cpu'
         # model
@@ -26,6 +27,70 @@ class ServerModel:
         processed_tensor = tensor * 2
 
         return jsonify({"processed_tensor": processed_tensor.tolist()})
+    
+    def generate_to_client(self)->str:
+        data = request.get_json()
+        prompt,max_len,num_return_sequences = str(data["prompt"]),int(data["max_length"]),int(data["num_return_sequences"])
+        input_ids = self._tokenizer.encode(prompt, return_tensors='pt').to(self._device)
+        output = self._model.generate(input_ids, max_length=max_len, num_return_sequences=num_return_sequences)
+        generated_text = self._tokenizer.decode(output[0], skip_special_tokens=True)
+        return jsonify({"generated_text": generated_text})
+    
+    def spec_tokens(self)->str:
+        seq_len = prefix.shape[1]
+        T = seq_len + max_len
+        
+        assert prefix.shape[0] == 1, "input batch size must be 1"
+
+        while prefix.shape[1] < T:
+            # p = M_p[prefix + x_0, x_1, .., x_(gamma-2)]
+            x = prefix
+            prefix_len = prefix.shape[1]
+            for _ in range(gamma):
+                # p.logits shape (batch, seq, vocab)
+                p = self._model(x).logits
+                next_tok = sample(norm_logits(p[:, -1, :], 
+                                temperature, top_k, top_p))
+                x = torch.cat((x, next_tok), dim=1)
+            
+            # normalize the logits
+            for i in range(p.shape[1]):
+                p[:,i,:] = norm_logits(p[:,i,:],
+                                temperature, top_k, top_p)
+            # q  = M_q[prefix + x_0, x_0, .., x_(gamma-1)]
+            q = self._spec_logits(x)
+            for i in range(q.shape[1]):
+                q[:,i,:] = norm_logits(q[:,i,:],
+                                temperature, top_k, top_p)
+
+            # n the end position of the valid prefix
+            # x = x_[:prefix_len-1] + x_0, ... x_(gamma-1)
+            
+            is_all_accept = True
+            n = prefix_len - 1
+            for i in range(gamma):
+                if random_seed:
+                    torch.manual_seed(random_seed)
+                r = torch.rand(1, device = self._device)
+                j = x[:, prefix_len + i]
+                
+                if r < torch.min(torch.tensor([1], device=self._device), q[:, prefix_len + i - 1, j] / p[:, prefix_len + i - 1, j]):
+                    # accept, and update n
+                    n += 1
+                else:
+                    # reject
+                    t = sample(max_fn(q[:, n, :] - p[:, n, :]))
+                    is_all_accept = False
+                    break
+        
+            prefix = x[:, :n + 1]
+            
+            if is_all_accept:
+                t = sample(q[:, -1, :])
+            
+            prefix = torch.cat((prefix, t), dim=1)
+            
+        return prefix
     
     def spec_logits(self):
         data = request.get_json()
